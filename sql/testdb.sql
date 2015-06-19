fae07f8fcd203bb1d6befb1edb2a8590c3ba98d8
root <root@dss.hom01>
2015-06-19 15:12:40 -0300

    Criação do dsiretório sql, onde ficarão todas as rotinas de inserção no banco. Arquivos '.sql'

---
+++ sql/testdb.sql
@@ -0,0 +1,12 @@
+create database if not exists nvtelecom;
+
+drop table ramal;
+
+create table ramal (
+id int(10) not null auto_increment,
+numero int(10) not null,
+nome varchar(100) not null,
+senha varchar(100) not null,
+primary key (id),
+unique(numero)
+);
