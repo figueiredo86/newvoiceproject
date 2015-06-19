d3dcc6fe869b72d393f8f6b6eaa1d8f43a696a7b
root <root@dss.hom01>
2015-06-19 14:37:29 -0300

    Criação do arquivo wrconfigfiles.php - Responsável pela funções de escrita dos arquivos de configuração

--- funcs/wrconfigfiles.php
+++ funcs/wrconfigfiles.php
@@ -1,12 +1,13 @@
 <?php
  function wrExtensions ($exten,$priority,$command,$cmdargs) {
     $cliname = "felipe";
-    $cliextlocation = "/home/felipe/scripts/newvoiceprojetc/astdirs/etc/asterisk/$cliname/extensions.conf";
-    $wrext = fopen($cliextlocation,"w") or die ("Impossivel abrir arquivo");
-    fwrite($wrext,"exten => ".$exten.",".$priority.",".$command."(".$cmdrgs.")");
+    $cliextlocation = "/home/felipe/scripts/newvoiceproject/astdirs/etc/asterisk/$cliname/extensions.conf";
+    $wrext = fopen($cliextlocation,"a+") or die ("Impossivel abrir arquivo");
+    fwrite($wrext,"exten => ".$exten.",".$priority.",".$command."(".$cmdargs.")"."\n");
     fclose($wrext);
  }
 
  wrExtensions("200",1,"NoOp","Hello World");
+ wrExtensions("200","n","Dial","SIP/200");
 
 ?>
