b1559fdec014d4b4a7198cc04359ed30dc3409ef
root <root@andromeda>
2015-11-05 13:14:47 -0200

    Alteracao geral de arquivos

--- www/html/main/paginas/p_clientes.php
+++ www/html/main/paginas/p_clientes.php
@@ -1,7 +1,7 @@
 <php
 	<html>
 	<head><title>Lista de clientes</title></head>
-	<body>Lista de cliente ativos da NVT</body>
+	<body>Lista de cliente ativos da NVT<br></body>
 	</html>
 
 	print "Esse cliente...";
