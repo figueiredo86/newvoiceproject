89b8e222dc3c2a7c42b3bdb4dc056bae95a3a215
root <root@andromeda>
2015-11-05 13:04:59 -0200

    Alteracao geral de arquivos

--- funcs/controldbcon.php
+++ funcs/controldbcon.php
@@ -1,21 +1,21 @@
 <?php
- include_once("../ini/config.ini.php");
- function conecta() { 
- $myconn = mysql_connect("$db[host]","$db[username]","$db[password]");
-  if (!$myconn) {
-   die('Erro ao conectar na base');
-  }
- }
+include_once("../ini/config.ini.php");
+function conecta() { 
+	$myconn = mysql_connect("$db[host]","$db[username]","$db[password]");
+       if (!$myconn) {
+  		die('Erro ao conectar na base');
+ 	}
+}
 
- function runquery($statement) {
-  conecta();
-  $run = mysql_query("$statement");
-  if (!$run) {
-   die('Nao foi possivel realizar a query');
-  }
+function runquery($statement) {
+	conecta();
+	$run = mysql_query("$statement");
+ 	if (!$run) {
+  		die('Nao foi possivel realizar a query');
+ 	}
 
-  while ($rows = mysql_fetch_array($run,MYSQL_BOTH)) {
-   printf("Nome: %s",$rows[0]);
-  }   
- }
+	 while ($rows = mysql_fetch_array($run,MYSQL_BOTH)) {
+ 		printf("Nome: %s",$rows[0]);
+ 	}   
+}
 ?>
